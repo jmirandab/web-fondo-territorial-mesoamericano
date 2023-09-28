e4877c8fe0a35bb4f2aa49de6f0a9716e7ab80e4
jmirandab <62623781+jmirandab@users.noreply.github.com>
2023-09-28 13:09:23 -0600

    Add files via upload
    First

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+///import i18next from './src/config/initi18n' delete
+
+
+const nextConfig = {
+    // i18next,
+    // reactStrictMode: true, delete
+    // swcMinify: true,
+}
+
+module.exports = nextConfig
