34b76173e41b581993d916dbda83ef75721e4d67
jmirandab <62623781+jmirandab@users.noreply.github.com>
2023-10-04 19:13:17 -0600

    Add files via upload
    files

--- next.config.js
+++ next.config.js
@@ -3,9 +3,15 @@
 
 
 const nextConfig = {
-    // i18next,
-    // reactStrictMode: true, delete
-    // swcMinify: true,
+    // i18n: {
+    //     // These are all the locales you want to support in
+    //     // your application
+    //     locales: ['en', 'es'],
+    //     // This is the default locale you want to be used when visiting
+    //     // a non-locale prefixed path e.g. `/hello`
+    //     defaultLocale: 'en',
+       
+    //   },
 }
 
 module.exports = nextConfig
