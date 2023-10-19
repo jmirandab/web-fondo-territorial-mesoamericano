02932b9262ffed2aa62e5d49fcfd217d745cfaad
Progra <progra@Juans-MacBook-Pro.local>
2023-10-19 00:31:42 -0600

    fix 9

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
-/** @type {import('next').NextConfig} */
-///import i18next from './src/config/initi18n' delete
-
-
-const nextConfig = {
-    // i18n: {
-    //     // These are all the locales you want to support in
-    //     // your application
-    //     locales: ['en', 'es'],
-    //     // This is the default locale you want to be used when visiting
-    //     // a non-locale prefixed path e.g. `/hello`
-    //     defaultLocale: 'en',
-       
-    //   },
-}
-
-module.exports = nextConfig
+module.exports = {
+    // Other Next.js configurations...
+  
+    experimental: {
+      serverlessTrace: true, // Add this line
+    },
+  };
+  
+  
