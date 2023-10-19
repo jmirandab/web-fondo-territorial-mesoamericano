cdefc88eba24ee9c94d4f4c8ef16878977c87b6e
Progra <progra@Juans-MacBook-Pro.local>
2023-10-18 22:51:46 -0600

    backgrounds

--- src/components/SectionFinancingInstruments.tsx
+++ src/components/SectionFinancingInstruments.tsx
@@ -7,7 +7,7 @@ import { useTranslation } from '../app/i18n'
 const SectionFinancingInstruments= async ({lng}: {lng:string}) => {
 
   const { t } = await useTranslation(lng);
-  return (
+  return ( <>
     <Section className={Styles.sectionFinancingInstruments}>
       <div className={Styles.totalScreenBackgroung}>
         <h2> 
@@ -16,26 +16,30 @@ const SectionFinancingInstruments= async ({lng}: {lng:string}) => {
         <ol className={Styles.financingInstruments}>
 
           <li>
-            {t('section6.bullet1')}
+          <strong>{t('section6.bullet1')}</strong>
             <div>
               {t('section6.subBullet1')}
             </div>
           </li>
           <li>
-            {t('section6.bullet2')}
+            <strong>{t('section6.bullet2')}</strong>
             <div>
               {t('section6.subBullet2')}
             </div>
           </li>
           <li>
-            {t('section6.bullet3')}
+          <strong>{t('section6.bullet3')}</strong>
             <div>
               {t('section6.subBullet3')}
             </div>
           </li>
         </ol>
       </div>
+      
     </Section>
+    <div className={Styles.wave}>
+      </div>
+    </>
   );
 };
 
