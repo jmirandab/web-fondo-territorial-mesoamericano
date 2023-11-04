80e553c18b16ba2f32212a66e367d7e00f648bb6
System Administrator <root@Juans-MacBook-Pro.local>
2023-11-04 11:44:22 -0600

    Strings rationale 2

--- src/components/SectionRational.tsx
+++ src/components/SectionRational.tsx
@@ -21,12 +21,20 @@ const SectionRational = async ({ lng }: { lng: string }) => {
               <div className={Styles.scrollArea}>
                 <p>
                   {t('section4.text1_part1')}
-                  <span>{t('section4.text1_super')}</span>
+                  <span className={Styles.super}>{t('section4.text1_super')}</span>
                   {t('section4.text1_part2')}
                 </p>
-                <p>{t('section4.text2')}</p>
+                <p>
+                  {t('section4.text2_part1')}
+                  <span className={Styles.green}>{t('section4.text2_part2')}</span>
+                  {t('section4.text2_part3')}
+                </p>
+                <p>
+                  {t('section4.text3_part1')}
+                  <span className={Styles.green}>{t('section4.text3_part2')}</span>
+                </p>
 
-                <p className={Styles.small}>{t('section4.text3')}</p>
+                <p className={Styles.small}>{t('section4.text4')}</p>
               </div>
             </div>
           </div>
