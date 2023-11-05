b0e13bb43dcc3d885327e644035f11555b4f32d4
System Administrator <root@Juans-MacBook-Pro.local>
2023-11-05 17:26:43 -0600

    fix build 2

---
+++ src/app/[lng]/board/page.tsx
@@ -0,0 +1,17 @@
+
+import Main from '@/components/Main'
+import { useTranslation } from '../../i18n'
+import SectionBoard from '../../../components/SectionBoard'
+
+
+
+export default async function Home({ params: { lng } }: { params: { lng: string } }) {
+  // const { t } = useTranslation();
+  const { t } = await useTranslation(lng)
+  return (
+    <Main lng={lng}>
+      <SectionBoard lng={lng}/>
+      
+    </Main>
+  )
+}
