4177e3bcdee08cdfc75a14ea82b55abf8833c269
jmirandab <juan.miranda.hr9@gmail.com>
2024-02-03 16:48:01 -0600

    events

--- src/app/components/SectionDialogo.tsx
+++ src/app/components/SectionDialogo.tsx
@@ -25,11 +25,12 @@ const SectionDialogo = ({ lng }: { lng: string }) => {
             Posicionar al Fondo Territorial Mesoamericano (FTM) como una herramienta eficaz para agilizar el financiamiento territorial directo y generar impactos positivos.
 
             <h3>Objetivos Específicos:</h3>
-            Facilitar el intercambio de experiencias sobre el desarrollo del financiamiento territorial directo.
-            Presentar experiencias de organizaciones de Pueblos Indígenas y Comunidades Locales en la implementación del financiamiento territorial directo.
-            Actualizar sobre el compromiso del FTFG y los avances del financiamiento territorial directo en Mesoamérica.
-            Proponer ideas y acuerdos para avanzar en los objetivos del financiamiento climático en la región mesoamericana.
-
+            <ul>
+              <li>Facilitar el intercambio de experiencias sobre el desarrollo del financiamiento territorial directo. </li>
+              <li>Presentar experiencias de organizaciones de Pueblos Indígenas y Comunidades Locales en la implementación del financiamiento territorial directo. </li>
+              <li>Actualizar sobre el compromiso del FTFG y los avances del financiamiento territorial directo en Mesoamérica. </li>
+              <li>Proponer ideas y acuerdos para avanzar en los objetivos del financiamiento climático en la región mesoamericana. </li>
+            </ul>
             <h3>PARTICIPANTES:</h3>
             <ul>
               <li>Donantes bilaterales y filantrópicos del Grupo de Financiación de la Tenencia de los Bosques (FTFG).</li>
@@ -38,9 +39,8 @@ const SectionDialogo = ({ lng }: { lng: string }) => {
               <li>Organizaciones territoriales socias.</li>
               <li>Empresas privadas con programas efectivos de responsabilidad social empresarial en la región.</li>
             </ul>
-            <p>El evento es cerrado, pero se realizarán transmisiones en vivo de paneles, debates y presentaciones mediante la plataforma Zoom y se transmitirán en vivo en la página
-               <Link target="_blank" href="https://www.facebook.com/alianzabosques/">Facebook</Link>  y <Link  target="_blank" href="https://www.youtube.com/channel/UCpjaCSauXCxms3PLMxFkgEA">Youtube</Link> de la AMPB. En caso de no poder asistir en vivo, las sesiones quedarán grabadas en la página de <Link target="_blank" href="https://www.facebook.com/alianzabosques/">Facebook</Link> y <Link  target="_blank" href="https://www.youtube.com/channel/UCpjaCSauXCxms3PLMxFkgEA">Youtube</Link> de la Alianza Mesoamericana de Pueblos y Bosques.
-             
+            <p>El evento es cerrado, pero se realizarán transmisiones en vivo de paneles, debates y presentaciones mediante la plataforma Zoom y se transmitirán en vivo en la página <Link target="_blank" href="https://www.facebook.com/alianzabosques/">Facebook</Link>  y <Link target="_blank" href="https://www.youtube.com/channel/UCpjaCSauXCxms3PLMxFkgEA">Youtube</Link> de la AMPB. En caso de no poder asistir en vivo, las sesiones quedarán grabadas en la página de <Link target="_blank" href="https://www.facebook.com/alianzabosques/">Facebook</Link> y <Link target="_blank" href="https://www.youtube.com/channel/UCpjaCSauXCxms3PLMxFkgEA">Youtube</Link> de la Alianza Mesoamericana de Pueblos y Bosques.
+
             </p>
           </div>
         </div>
